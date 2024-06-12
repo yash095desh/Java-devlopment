62ee21795ff84cd2946a46c188db81e17e4ec526
Yash <yashdesh095@gmail.com>
2024-06-12 16:22:36 +0530

    completed operators

---
+++ src/basic2/Operators.java
@@ -0,0 +1,77 @@
+package basic2;
+
+public class Operators {
+    public static void main (String[] args){
+        // Operators
+        int a = 10;
+        int b = 5;
+
+
+        //1. Arithmatic Operators
+        /*
+        1) + -> addition
+        2) -  -> subtaction
+        3) * -> multiplicaton
+        4) / => division
+        5) % => modulus
+        */
+
+        int c = a + b ;
+        System.out.println(c);
+        System.out.println(a*b);
+
+        // a -> operant , b -> operant , + -> operator
+
+        //2. Assignment Operator
+
+         /*1) =   a = b;
+         2) +=  a += b;
+         3) -=  a-=b;
+         4) *=  a*=b;
+         5) /=  a/=b;
+         6) %=  a%=b;*/
+
+        /*System.out.println(a = b);
+        System.out.println( a+=b);
+        System.out.println( a-=b);
+        System.out.println( a*=b);
+        System.out.println( a/=b);
+        System.out.println( a%=b);*/
+
+
+        // 3. Relational Operators
+
+       /* 1) ==  --> Equals to
+        2) !=  --> Not Equal to
+        3) <   --> Less Than
+        4) >  --> Greater Than
+        5) <= --> Less than equal to
+        6) >= --> Greater than equal to*/
+
+       /* System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println( a<b);
+        System.out.println( 3 >= 3);
+        System.out.println(a>=b);
+        System.out.println(a<=b);*/
+
+        //4. Logical Operators
+
+        /*1)&&(Logical and )
+        2)||(Logical or)
+        3)!(Logical Not)*/
+
+     /*   System.out.println(true && false);
+        System.out.println(true && true);
+        System.out.println(true || false);
+        System.out.println(true || true);
+        System.out.println(false || false);
+        */
+
+
+
+
+
+    }
+}
