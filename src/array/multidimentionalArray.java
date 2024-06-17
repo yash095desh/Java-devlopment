98b49775431d70dd3cbd6d6dbc088b570745a131
Yash <yashdesh095@gmail.com>
2024-06-17 22:23:15 +0530

    completed multidimentional array

---
+++ src/array/multidimentionalArray.java
@@ -0,0 +1,21 @@
+package array;
+
+public class multidimentionalArray {
+    public static void main (String[] args){
+//        // Multidimentional array is used to store similar datatype data in 2D structure
+//        int arr [][] = new int [2][3];
+//        arr[0][0] = 20;
+//        arr[0][1] = 12;
+//        System.out.println(arr[0][0]);
+//        System.out.println(arr[0][1]);
+//        // or we can insert values directly
+
+//        int newarr[][] = {{12,23,44},{22,32,55}};
+//        System.out.print(newarr[0][0]+" " );
+//        System.out.print(newarr[0][1]+" ");
+//        System.out.println(newarr[0][2]);
+//        System.out.print(newarr[1][0]+" ");
+//        System.out.print(newarr[1][1]+" ");
+//        System.out.print(newarr[1][2]+" ");
+    }
+}
