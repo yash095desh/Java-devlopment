00daba6bb7dc03aa86325282033629ed907de340
Yash <yashdesh095@gmail.com>
2024-06-20 11:26:11 +0530

    learned final keyword usecases

---
+++ src/oops2/finalKeyWord.java
@@ -0,0 +1,14 @@
+package oops2;
+
+// class with final keyword cannot be extended or inherited from other classes
+
+//public class finalKeyWord extends Car{
+
+//}
+
+
+public class finalKeyWord {
+    final int number = 5;
+    // Variable with final keyword cannot assigned again
+//    number = 10 ;
+}
